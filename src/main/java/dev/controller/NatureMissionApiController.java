afba80b950875fc78aba4185af2381b388dfca6a
Mayeul Grivet-Roux <gmayeul@yahoo.fr>
2018-08-20 16:27:43 +0200

    CRUD - Delete nature de mission

--- src/main/java/dev/controller/NatureMissionApiController.java
+++ src/main/java/dev/controller/NatureMissionApiController.java
@@ -1,5 +1,9 @@
 package dev.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +18,16 @@ public class NatureMissionApiController {
 	public NatureMissionApiController(NatureMissionService natureMissionService) {
 		super();
 		this.natureMissionService = natureMissionService;
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> supprimer(@PathVariable Long id) {
+		if (natureMissionService.exist(id)) {
+			natureMissionService.deleteById(id);
+			return ResponseEntity.status(HttpStatus.OK).body("Nature de mission correctement supprimée");
+		} else {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Cette nature de mission n'existe pas");
+		}
 	}
 
 }
